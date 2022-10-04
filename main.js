1785a11bd7d167916458df512828894582d92f45
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2022-10-04 18:03:01 +0900

    add the random card

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ card.addEventListener('click', function(){
     card.classList.toggle('is-flipped');
 });
 
+const card_1 = document.querySelector('.card__inner_1');
+
+card.addEventListener('click', function(){
+    card.classList.toggle('is-flipped1');
+});
